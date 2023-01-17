6eab0ae5e5b74ca26097043151f2cda9054a9c04
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-17 00:52:11 -0300

    Instalando prisma no projeto e realizando conexão com o banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,17 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
 app.get('/hello', () => {
   return 'Hello World'
 })
 
-app.listen({
-  port: 3333
-})
+app
+  .listen({
+    port: 3333
+  })
+  .then(() => {
+    console.log('Server is running on port 3333')
+  })
