c19760c307445010143c18e90c812b7cc3a53675
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-17 22:09:09 -0300

    Fazendo pequenas estilizações

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -13,9 +13,7 @@ export default function App() {
   })
 
   if (!fontsLoaded) {
-    return (
-      <Loading />
-    )
+    return <Loading />
   }
 
   return (
@@ -34,6 +32,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   text: {
-    color: 'red'
+    color: '#fff',
+    fontFamily: 'Inter_800ExtraBold'
   }
 })
