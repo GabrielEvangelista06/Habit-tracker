39567a2470bdf79f4c8f8bfbb4483d71d5bb271e
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-19 00:09:55 -0300

    Criando e finalizando rota que muda o status de um hábito

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs'
 import { FastifyInstance } from 'fastify'
+import { request } from 'http'
 import { z } from 'zod'
 import { prisma } from './lib/prisma'
 
@@ -70,6 +71,60 @@ export async function appRoutes(app: FastifyInstance) {
     return {
       posbibleHabits,
       completedHabits
+    }
+  })
+
+  // completar / não comppletar uma tarefa
+
+  app.patch('/habits/:id/toggle', async request => {
+    // route param => parâmetro de identificação
+
+    const toggleHabitsParam = z.object({
+      id: z.string().uuid()
+    })
+
+    const { id } = toggleHabitsParam.parse(request.params)
+
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id
+        }
+      }
+    })
+
+    if (dayHabit) {
+      // remover a marcação de completo
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id
+        }
+      })
+    } else {
+      // Completar o hábito
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id
+        }
+      })
     }
   })
 }
