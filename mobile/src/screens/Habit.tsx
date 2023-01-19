857ad6089125be9d1c44a65d5e1a29483e35b9fe
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-19 17:33:04 -0300

    Ultilizando component Checkbox na tela de hábito

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -4,6 +4,7 @@ import dayjs from 'dayjs'
 
 import { BackButton } from '../components/BackButton'
 import { ProgressBar } from '../components/Progressbar'
+import { Checkbox } from '../components/Checkbox'
 
 interface Params {
   date: string
@@ -27,6 +28,11 @@ export function Habit() {
         <Text className="text-white font-extrabold text-3xl">{dayAndMonth}</Text>
 
         <ProgressBar progress={30} />
+
+        <View className="mt-6">
+          <Checkbox title="Beber 2L de água" checked={false} />
+          <Checkbox title="Caminhar" checked={true} />
+        </View>
       </ScrollView>
     </View>
   )
