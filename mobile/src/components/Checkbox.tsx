f26cacdc1e2ba8cb4fb9ae2909903d017b0ab052
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-19 17:03:24 -0300

    Finalizando component Checkbox

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,15 +1,15 @@
-import { TouchableOpacity, View, Text } from 'react-native'
+import { TouchableOpacity, TouchableOpacityProps, View, Text } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
 
-interface Props {
+interface Props extends TouchableOpacityProps {
   title: string
   checked?: boolean
 }
 
-export function Checkbox({ title, checked = false }: Props) {
+export function Checkbox({ title, checked = false, ...rest }: Props) {
   return (
-    <TouchableOpacity activeOpacity={0.7} className="flex-row mb-2 items-center">
+    <TouchableOpacity activeOpacity={0.7} className="flex-row mb-2 items-center" {...rest}>
       {checked ? (
         <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
           <Feather name="check" size={20} color={colors.white} />
