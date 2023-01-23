2ba1b500cf67a535785e86c455515b33005b1e43
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-23 01:17:49 -0300

    Verificando se a data selecionada é uma data passada e mostrar mensgame de que os hábitos desse dia não podem ser editados

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -11,6 +11,7 @@ import { ProgressBar } from '../components/Progressbar'
 import { Checkbox } from '../components/Checkbox'
 import { Loading } from '../components/Loading'
 import { HabitsEmpty } from '../components/HabitsEmpty'
+import clsx from 'clsx'
 
 interface Params {
   date: string
@@ -33,6 +34,7 @@ export function Habit() {
   const { date } = route.params as Params
 
   const parsedDate = dayjs(date)
+  const isDateInPast = parsedDate.endOf('day').isBefore(new Date())
   const dayOfWeek = parsedDate.format('dddd')
   const dayAndMonth = parsedDate.format('DD/MM')
 
@@ -81,15 +83,27 @@ export function Habit() {
 
         <ProgressBar progress={habitsProgress} />
 
-        <View className="mt-6">
+        <View
+          className={clsx('mt-6', {
+            ['opacity-50']: isDateInPast
+          })}
+        >
           {dayInfo?.possibleHabits ? (
             dayInfo?.possibleHabits.map(habit => (
-              <Checkbox key={habit.id} title={habit.title} checked={completedHabits.includes(habit.id)} onPress={() => handleToggleHabit(habit.id)} />
+              <Checkbox
+                key={habit.id}
+                title={habit.title}
+                checked={completedHabits.includes(habit.id)}
+                disabled={isDateInPast}
+                onPress={() => handleToggleHabit(habit.id)}
+              />
             ))
           ) : (
             <HabitsEmpty />
           )}
         </View>
+
+        {isDateInPast && <Text className="text-white mt-10 text-center">Você não pode editar hábitos de uma data passada.</Text>}
       </ScrollView>
     </View>
   )
